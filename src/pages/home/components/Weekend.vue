156a9d55d0350051a3c3a53e8d1d786ed5c86da5
dyy <1002231630@qq.com>
2018-05-21 18:12:21 +0800

    add recommend

---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,73 @@
+<template>
+	<div>
+		<div class="titile">周末去哪儿</div>
+		<ul>
+			<li 
+				class="item border-bottom" 
+				v-for="item of recomendList" 
+				:key="item.id"
+			>
+				<div class="item-img-wrapper">
+					<img class="item-img" :src="item.imgUrl" alt="">
+				</div>
+				<div class="item-info">
+					<p class="item-title">{{item.title}}</p>
+					<p class="item-desc">{{item.desc}}</p>
+				</div>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'HomeRecomend',
+		data (){
+			return {
+				recomendList: [{
+					id: '0001',
+					imgUrl: 'http://img1.qunarzz.com/sight/source/1505/aa/7baaf8a851d221.jpg_r_640x214_1431200f.jpg',
+					title: '北京欢乐谷',
+					desc: '52817条评论'
+				}, {
+					id: '0002',
+					imgUrl: 'http://img1.qunarzz.com/sight/source/1505/aa/7baaf8a851d221.jpg_r_640x214_1431200f.jpg',
+					title: '北京欢乐谷',
+					desc: '52817条评论'
+				}, {
+					id: '0003',
+					imgUrl: 'http://img1.qunarzz.com/sight/source/1505/aa/7baaf8a851d221.jpg_r_640x214_1431200f.jpg',
+					title: '北京欢乐谷',
+					desc: '52817条评论'
+				}]
+			}
+			
+		}
+	};
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+	.titile
+		line-height: .8rem
+		background: #eee
+		text-indent: .2rem
+	.item-img-wrapper
+		overflow: hidden
+		height: 0
+		padding-bottom: 33.9%
+		.item-img
+			width: 100%
+	.item-info
+		flex: 1
+		padding: .1rem
+		min-width: 0
+		.item-title
+			line-height: .54rem
+			font-size: .32rem
+			ellipsis()
+		.item-desc
+			line-height: .4rem
+			color: #ccc
+			ellipsis()
+</style>
